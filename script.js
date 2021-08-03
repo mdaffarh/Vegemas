35c67e289e82895b1cff58a2dd42bf992d05db44
mdaffarh <daffarizmawan@gmail.com>
2021-08-03 20:38:17 +0700

    add location in contact section

--- script.js
+++ script.js
@@ -10,10 +10,6 @@ menuToggle.addEventListener("click", function () {
 
 const scriptURL = "https://script.google.com/macros/s/AKfycbxQQ4psgr8CUaOcykazg1TH5NBI9e5-qdIaxhbnCu6_ezPSoAwcMow-68BYS19WDqiX5g/exec";
 const form = document.forms["contact-form"];
-const btnSend = document.querySelector(".btn-send");
-const btnLoading = document.querySelector(".btn-loading");
-const myAlert = document.querySelector(".my-alert");
-const btnClose = document.querySelector(".btn-close");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
